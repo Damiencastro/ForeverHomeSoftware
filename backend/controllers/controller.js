58cea40dcb7f083472a7dfd9882ccb5421b01e5f
David Castro <github@dvcastro.com>
2025-03-28 18:15:41 -0400

    Adds more endpoints with mock data

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -11,7 +11,7 @@ router.use((req, res, next) => {
 });
 
 // Get all rescue groups
-router.get('/rescue-groups', async (req, res) => {
+router.get('/rescue-group', async (req, res) => {
     const mockData = [
         { id: 1, name: "Wine Country Greyhounds"},
         { id: 1, name: "Blessed Bow Wows"},
@@ -21,7 +21,7 @@ router.get('/rescue-groups', async (req, res) => {
 })
 
 // Get all fosters
-router.get('/fosters', async (req, res) => {
+router.get('/foster', async (req, res) => {
     /* Add database call later
     const animals = await service.getAllFosters()
     res.send(fosters)
@@ -29,10 +29,57 @@ router.get('/fosters', async (req, res) => {
     const mockData = [
         { id: 1, name: "Fluffy", image: "https://dog.ceo/api/breeds/image/random", age: "Puppy", breed: "Coon hound", gender: "Female", size: "Medium", description: "Loves to play fetch", energy: "Medium", childsafe: "Good with kids over 2", dogsafe: "No", catsafe: "No", housetrained: "Yes" },
         { id: 2, name: "Spot", image: "https://dog.ceo/api/breeds/image/random", age: "Adult", breed: "Puggle", gender: "Male", size: "Small", description: "Sleeps most of the day", energy: "Low", childsafe: "Good with kids over 2", dogsafe: "No", catsafe: "No", housetrained: "Yes" },
-        { id: 3, name: "Fluffy", image: "https://dog.ceo/api/breeds/image/random", age: "Senior", breed: "Greyhound", gender: "Female", size: "Large", description: "Runs 100MPH or 0MPH. Not much between.", energy: "Couch potato", childsafe: "Good with kids over 10", dogsafe: "Yes", catsafe: "Yes", housetrained: "Yes" },
+        { id: 3, name: "Sir Barksalot", image: "https://dog.ceo/api/breeds/image/random", age: "Senior", breed: "Greyhound", gender: "Female", size: "Large", description: "Runs 100MPH or 0MPH. Not much between.", energy: "Couch potato", childsafe: "Good with kids over 10", dogsafe: "Yes", catsafe: "Yes", housetrained: "Yes" },
     ]
     res.json(mockData);
 })
 
+// Get all age labels
+router.get('/age', async (req, res) => {
+    const mockData = [
+        {id: 1, label: "Puppy"},
+        {id: 2, label: "Adult"},
+        {id: 3, label: "Senior"}
+    ]
+    res.json(mockData);
+})
+
+// Get all childsafe labels
+router.get('/childsafe', async (req, res) => {
+    const mockData = [
+        {id: 1, label: "Unknown"},
+        {id: 2, label: "Not safe with preteen kids"},
+        {id: 3, label: "Safe with kids of all ages"},
+        {id: 4, label: "Safe with kids over 2"},
+        {id: 5, label: "Safe with kids over 5"},
+        {id: 6, label: "Safe with kids over 10"}
+    ]
+    res.json(mockData);
+})
+
+// Get all energy labels
+router.get('/energy', async (req, res) => {
+    const mockData = [
+        {id: 1, label: "Unknown"},
+        {id: 2, label: "Couch potato"},
+        {id: 3, label: "Energetic every now and again"},
+        {id: 4, label: "Needs exercise some days"},
+        {id: 5, label: "Needs exercise every day"},
+        {id: 6, label: "Fireball"}
+    ]
+    res.json(mockData);
+})
+
+// Get all dogsafe labels
+router.get('/dogsafe', async (req, res) => {
+    const mockData = [
+        {id: 1, label: "Unknown"},
+        {id: 2, label: "Best for a one-dog household"},
+        {id: 3, label: "Safe with dogs of similar size"},
+        {id: 4, label: "Safe with most other dogs"},
+        {id: 5, label: "Would do better in a house with other dogs"}
+    ]
+    res.json(mockData);
+})
 
 module.exports = router
